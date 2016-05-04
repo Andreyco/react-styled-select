906328d6c61caf3b87a521f79d07f36d9eb96a9b
Andrej Badin <contact@andrejbadin.com>
2016-05-04 14:31:08 +0200

    Update example

--- examples/index.js
+++ examples/index.js
@@ -8,23 +8,4 @@ for(const country of countries) {
   options.push(<option key={country.name} value={country.name}>{country.name}</option>);
 }
 
-const style = {
-  // width: 200,
-  // maxWidth: 200,
-  // position: 'absolute',
-  // top: '50%',
-  // left: '50%',
-  // transform: 'translateY(-50%) translateX(-50%)',
-};
-
-const App = React.createClass({
-  render() {
-    return (
-      <div>
-        <StyledSelect onBlur={() => {}} children={options}  style={style}/>
-      </div>
-    );
-  }
-})
-
-render(<App />, document.getElementById('root'));
+render(<StyledSelect children={options}/>, document.getElementById('root'));
