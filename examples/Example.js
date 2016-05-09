251bfc4ae5436910958b82df5afe9f3d6339bea6
Andrej Badin <contact@andrejbadin.com>
2016-05-09 23:13:25 +0200

    Update examples

--- examples/Example.js
+++ examples/Example.js
@@ -14,20 +14,6 @@ for (let i = year; i > year - 10; i--) {
 	optionsYears.push(<option key={i} value={i}>{i}</option>);
 };
 
-/**
- * Styling
- */
-const group = {
-	marginBottom: 50,
-};
-
-const overflow = {
-	border: '1px solid #ccc',
-	padding: 10,
-	height: 10,
-	overflow: 'hidden',
-}
-
 const Example = (props) => (
 	<div>
 		<header className="gradient">
@@ -45,69 +31,74 @@ const Example = (props) => (
 
 		<div style={{maxWidth: 400, margin: '0 auto', padding: '0 25px'}}>
 			<p>
-				React Styled Select makes styling &lt;select&gt; element&nbsp;easy.
+				React Styled Select makes styling <i>&lt;select&gt;</i> element&nbsp;easy.
 			</p>
 			<p style={{marginBottom: 50}}>
 				It leaves rendering of &lt;option&gt; dropdown completely to browser, thus
 				long dropdowns are problem-free and overflows are not hidden.
 			</p>
 
-			<h2>Examples</h2>
-			<div style={group}>
-				<b>Long dropdown</b>
-				<small>
-					What's your home country? Pick one from this very long dropdown.
-				</small>
-				<StyledSelect defaultValue="Slovakia">
-					{ optionsCountries }
-				</StyledSelect>
-			</div>
-
-			<div style={group}>
-				<b>Disabled</b>
-				<StyledSelect defaultValue="Slovakia" disabled>
-					{ optionsCountries }
-				</StyledSelect>
-			</div>
-
-			<div style={group}>
-				<b>Overflows</b>
-				<small>
-					Overflowing content outside of these borders is cropped. Open it, dropdown is displayed properly.
-					Other custom solutions makes displaying dropdown impossible without using "portals".
-				</small>
-				<div style={overflow} className="gradient">
-					<StyledSelect>
-						{ optionsYears }
+			<section>
+				<h2>Examples</h2>
+				<div className="group">
+					<b>Long dropdown</b>
+					<small>
+						What's your home country? Pick one from this very long dropdown.
+					</small>
+					<StyledSelect defaultValue="Slovakia">
+						{ optionsCountries }
 					</StyledSelect>
 				</div>
-			</div>
 
-			<h2>Features</h2>
-			<div style={group}>
-				<b>Drop-in solution</b>
-				<small>
-					Replace &lt;select&gt; with &lt;StyledSelect&gt; &mdash; it just works.
-				</small>
-			</div>
+				<div className="group">
+					<b>Disabled</b>
+					<StyledSelect defaultValue="Slovakia" disabled>
+						{ optionsCountries }
+					</StyledSelect>
+				</div>
 
-			<div style={group}>
-				<b>Focusable</b>
-				<small>
-					React Styled Select receives focus. Press `tab` or `shift + tab` button
-					to navigate thru all React Styled Select elements on page.
-				</small>
-			</div>
+				<div className="group">
+					<b>Overflows</b>
+					<small>
+						Overflowing content outside of these borders is cropped. Open it, dropdown is displayed properly.
+						Other custom solutions makes displaying dropdown impossible without using "portals".
+					</small>
+					<div className="gradient overflow-wrapper">
+						<StyledSelect>
+							{ optionsYears }
+						</StyledSelect>
+					</div>
+				</div>
+			</section>
 
-			<div style={group}>
-				<b>Keyboard friendly</b>
-				<small>
-					Navigating and selecting value from dropdown can be done using keyboard only.
-				</small>
-			</div>
+			<section>
+				<h2>Features</h2>
+				<div className="group">
+					<b>Drop-in solution</b>
+					<small>
+						Replace <i>&lt;select&gt;</i> with <i>&lt;ReactStyledSelect&gt;</i> &mdash; it just works.
+					</small>
+				</div>
+
+				<div className="group">
+					<b>Focusable</b>
+					<small>
+						React Styled Select receives focus. Press `tab` or `shift + tab` button
+						to navigate thru all React Styled Select elements on page.
+					</small>
+				</div>
+
+				<div className="group">
+					<b>Keyboard friendly</b>
+					<small>
+						Navigating and selecting value from dropdown can be done using keyboard only.
+					</small>
+				</div>
+			</section>
 		</div>
+
 		<footer>
-			<small>Copyright © <b>Andrej Badin</b> 2016. MIT Licensed.</small>
+			<small>Copyright © <b>Andrej Badin</b> { (new Date()).getFullYear() }. MIT Licensed.</small>
 		</footer>
 	</div>
 );
--- examples/webpack.config.prod.js
+++ examples/webpack.config.prod.js
@@ -4,17 +4,21 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
   ],
   externals: {
     'react': 'React',
